adebe4bf205ee185f69478c3edfc225e63ba1896
ashandilya-umass <165324105+ashandilya-umass@users.noreply.github.com>
2024-04-09 15:07:12 -0400

    pyscript_test.py

--- pyscript_test.py
+++ pyscript_test.py
@@ -1,32 +1,14 @@
-#Using Flask:
-#from flask import Flask, render_template
-#
-#app = Flask(__name__)
-#
-#@app.route('/')
-#def home():
-#    return 'Hello, World!'
-#
-#@app.route('/about')
-#def about():
-#    return 'This is the about page'
-#
-#if __name__ == '__main__':
-#    app.run(debug=True)
-
-
-#Using Pyscript?:
 from pyscript import document
 
 
-def greet_person(name='cole'):
+def drug(name='Aducanumab'):
     #name = input("Enter your name: ")
-    return f"Hello, {name}!"
+    return name
 
 # Call the function
-greeting = greet_person()
+output = drug()
 
 # Print the greeting
 def run_program(event):
     output_div = document.querySelector("#output")
-    output_div.innerText = greeting
+    output_div.innerText = output

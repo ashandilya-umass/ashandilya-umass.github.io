4041381eb18b0307f9217cadd1ef8c98eda3ece2
ashandilya-umass <165324105+ashandilya-umass@users.noreply.github.com>
2024-04-16 18:07:53 -0400

    Update pyscript_test.py

--- pyscript_test.py
+++ pyscript_test.py
@@ -3,7 +3,7 @@ from pyscript import document
 def run_program(event):
     # Get the input field value
     user_input = document.getElementById("name").value
-    greeting = f'Hello, {user_input}!'
+    greeting = f'Hello, {user_input}! Welcome'
     
     # Display the greeting in the output div
     output_div = document.getElementById("output")

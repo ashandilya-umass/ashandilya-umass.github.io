1b3604e8da93b9b09ff4dbc56c9a76f5d0baa151
ashandilya-umass <ashandilya@umass.edu>
2024-04-21 19:52:33 -0400

    Update pyscript_test.py

--- pyscript_test.py
+++ pyscript_test.py
@@ -1,6 +1,6 @@
 from pyscript import document
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 # import pandas as pd
 # import webbrowser
 # import rdkit

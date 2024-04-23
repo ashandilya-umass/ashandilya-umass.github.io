f8b7b8040887c56d600014e92f068262723f48cf
ashandilya-umass <165324105+ashandilya-umass@users.noreply.github.com>
2024-04-23 17:23:33 -0400

    Update pyscript_test.py
    using wikipedia api instead of url

--- pyscript_test.py
+++ pyscript_test.py
@@ -9,6 +9,7 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 from bs4 import BeautifulSoup
 import pandas as pd
+import wikipediaapi
 # import webbrowser
 # import rdkit
 # from rdkit import Chem
@@ -76,22 +77,37 @@ def run_program(event):
             # user_input_split = confirmation(disease)
             word = disease
             
-            url = website_links[word]
-            response = requests.get(url, verify = True)
+            # url = website_links[word]
+            # response = requests.get(url, verify = True)
 
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.text, 'html.parser')
-                title = soup.title.string
-                output = f'Title: {title}'
-                soup = BeautifulSoup(response.text, 'html.parser')
-                paragraphs = soup.find_all('p')
-                info_text = "\n".join(paragraph.get_text() for paragraph in paragraphs)
-                output = output + '\n' + info_text + '\n' + f'Website Link: {url}'
-                # print("Title:", title)
-            else:
-                # print("Failed to retrieve the webpage. Status code:", response.status_code)
-                output = f'Failed to retrieve the webpage. Status code: {response.status_code}'
-                
+            # if response.status_code == 200:
+            #     soup = BeautifulSoup(response.text, 'html.parser')
+            #     title = soup.title.string
+            #     output = f'Title: {title}'
+            #     soup = BeautifulSoup(response.text, 'html.parser')
+            #     paragraphs = soup.find_all('p')
+            #     info_text = "\n".join(paragraph.get_text() for paragraph in paragraphs)
+            #     output = output + '\n' + info_text + '\n' + f'Website Link: {url}'
+            #     # print("Title:", title)
+            # else:
+            #     # print("Failed to retrieve the webpage. Status code:", response.status_code)
+            #     output = f'Failed to retrieve the webpage. Status code: {response.status_code}'
+
+            wiki_wiki = wikipediaapi.Wikipedia(
+                user_agent='MyProjectName (merlin@example.com)',
+                    language='en',
+                    extract_format=wikipediaapi.ExtractFormat.WIKI
+            )
+            
+            p_wiki = wiki_wiki.page(word)
+            # print(p_wiki.text)
+            
+            wiki_text = p_wiki.text
+            
+            paras = wiki_text.split('\n')
+
+            output = paras[0]
+                            
     output_div = document.getElementById("output")
     output_div.textContent = output
 

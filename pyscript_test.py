bedaa82a794d7f65f2c33c72a61879b1a83c4e77
ashandilya-umass <ashandilya@umass.edu>
2024-04-30 16:24:56 -0400

    Update pyscript_test.py

--- pyscript_test.py
+++ pyscript_test.py
@@ -252,12 +252,13 @@ website_links = {'alzheimer': "https://en.wikipedia.org/wiki/Alzheimer%27s_disea
 
 async def run_program(event):
     user_input = document.getElementById("name").value.lower()
+    output_div = document.getElementById("output")  # Define output_div here
     if user_input in website_links:
         url = website_links[user_input]
-        output_div = document.getElementById("output")
         output_div.textContent = f"For more information, click <a href='{url}' target='_blank'>here</a>."
     else:
         output_div.textContent = f"{user_input.capitalize()} not found in the dictionary."
 
 document.getElementById("name").addEventListener("click", create_once_callable(run_program))
+
 
